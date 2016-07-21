cbe2db9fae14677cf57090bc6eddcde66fc42a69
mukilankarthi <mukilankarthikeyan@gmail.com>
2016-07-21 21:38:32 +0530

    removed method level comments for model class cart

--- model/Cart.java
+++ model/Cart.java
@@ -4,8 +4,10 @@
 package model;
 
 /**
- * @author ideas2it
+ * @author Mukilan.K
  *
+ * @version 1.0
+ * <p> Cart class acts as a model which represents cart details
  */
 public class Cart {
     private int id;
@@ -14,72 +16,42 @@ public class Cart {
     private int quantity;
     private double totalPrice;
 
-	/**
-	 * @return the id
-	 */
 	public int getId() {
 		return id;
 	}
 
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	/**
-	 * @return the orderId
-	 */
 	public int getOrderId() {
 		return orderId;
 	}
 
-	/**
-	 * @param orderId the orderId to set
-	 */
 	public void setOrderId(int orderId) {
 		this.orderId = orderId;
 	}
 
-	/**
-	 * @return the productId
-	 */
 	public int getProductId() {
 		return productId;
 	}
 
-	/**
-	 * @param productId the productId to set
-	 */
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
 
-	/**
-	 * @return the quantity
-	 */
 	public int getQuantity() {
 		return quantity;
 	}
 
-	/**
-	 * @param quantity the quantity to set
-	 */
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
 
-	/**
-	 * @return the totalPrice
-	 */
 	public double getTotalPrice() {
 		return totalPrice;
 	}
 
-	/**
-	 * @param totalPrice the totalPrice to set
-	 */
 	public void setTotalPrice(double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
