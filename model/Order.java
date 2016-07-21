475fb4c9516fd7ceb1e6bb96116d83c1f62360dc
[mukilankarthi] <[mukilankarthikeyan@gmail.com]>
2016-07-21 20:08:31 +0530

    Feature: added pojo classes for Role,UserRole,Cart,Order

---
+++ model/Order.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author ideas2it
+ *
+ */
+public class Order {
+    private int id;
+    private int userId;
+    private double total;
+    private String paymentType;
+    private boolean status;
+    
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the total
+	 */
+	public double getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	/**
+	 * @return the paymentType
+	 */
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	/**
+	 * @param paymentType the paymentType to set
+	 */
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public boolean isStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+}
