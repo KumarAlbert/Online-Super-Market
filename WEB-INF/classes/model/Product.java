52f9502209ebf35be0535b97ba88cf29e80b3876
[KumarAlbert] <[kumar@ideas2it.com]>
2016-07-21 16:29:32 +0530

    added pojo classes for user,category,subcategory,product

---
+++ WEB-INF/classes/model/Product.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Kumar Albert
+ *
+ * version 1.0
+ * 
+ */
+public class Product {
+	private int id;
+	private String name;
+	private String Description;
+	private int subcategoryId;
+	private String imageUrl;
+	private int stock;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return Description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		Description = description;
+	}
+	/**
+	 * @return the subcategoryId
+	 */
+	public int getSubcategoryId() {
+		return subcategoryId;
+	}
+	/**
+	 * @param subcategoryId the subcategoryId to set
+	 */
+	public void setSubcategoryId(int subcategoryId) {
+		this.subcategoryId = subcategoryId;
+	}
+	/**
+	 * @return the imageUrl
+	 */
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	/**
+	 * @param imageUrl the imageUrl to set
+	 */
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	/**
+	 * @return the stock
+	 */
+	public int getStock() {
+		return stock;
+	}
+	/**
+	 * @param stock the stock to set
+	 */
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+}
---
+++ WEB-INF/classes/model/User.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Kumar Albert
+ *
+ * version 1.0
+ * 
+ */
+public class User {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private long mobileNumber;
+	private String password;
+	private String address;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the mobileNumber
+	 */
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+	/**
+	 * @param mobileNumber the mobileNumber to set
+	 */
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
