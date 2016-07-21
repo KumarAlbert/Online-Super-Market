e98da813500157ec028bad3de91f1a65874ff0e0
mukilankarthi <mukilankarthikeyan@gmail.com>
2016-07-21 21:41:41 +0530

    removed method level comments for class Order

--- model/Order.java
+++ model/Order.java
@@ -4,8 +4,10 @@
 package model;
 
 /**
- * @author ideas2it
+ * @author Mukilan.K
  *
+ * @version 1.0
+ * <p>Order class acts as a model which represents Order details>
  */
 public class Order {
     private int id;
@@ -13,73 +15,43 @@ public class Order {
     private double total;
     private String paymentType;
     private boolean status;
-    
-	/**
-	 * @return the id
-	 */
+
 	public int getId() {
 		return id;
 	}
 
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	/**
-	 * @return the userId
-	 */
 	public int getUserId() {
 		return userId;
 	}
 
-	/**
-	 * @param userId the userId to set
-	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 
-	/**
-	 * @return the total
-	 */
 	public double getTotal() {
 		return total;
 	}
 
-	/**
-	 * @param total the total to set
-	 */
 	public void setTotal(double total) {
 		this.total = total;
 	}
 
-	/**
-	 * @return the paymentType
-	 */
 	public String getPaymentType() {
 		return paymentType;
 	}
 
-	/**
-	 * @param paymentType the paymentType to set
-	 */
 	public void setPaymentType(String paymentType) {
 		this.paymentType = paymentType;
 	}
 
-	/**
-	 * @return the status
-	 */
 	public boolean isStatus() {
 		return status;
 	}
 
-	/**
-	 * @param status the status to set
-	 */
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
