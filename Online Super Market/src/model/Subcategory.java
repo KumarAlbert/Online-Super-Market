e90315555c572c0333bc7d56e8d7fdfc55693a9f
[KumarAlbert] <[kumar@ideas2it.com]>
2016-07-21 18:57:39 +0530

    add all pojo classes and mysql query

---
+++ Online Super Market/src/model/Subcategory.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Kumar Albert
+ *
+ * version 1.0
+ * 
+ */
+public class Subcategory {
+	private int id;
+	private String name;
+	private int categoryId;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the categoryId
+	 */
+	public int getCategoryId() {
+		return categoryId;
+	}
+	/**
+	 * @param categoryId the categoryId to set
+	 */
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+}
