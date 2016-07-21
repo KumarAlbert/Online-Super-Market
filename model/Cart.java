a6a7156c70f4f7a364daaba36d6bec989959fe64
[mukilankarthi] <[mukilankarthikeyan@gmail.com]>
2016-07-21 20:18:36 +0530

    Feature: added pojo classes for Role,UserRole,Cart,Order

---
+++ model/Cart.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author ideas2it
+ *
+ */
+public class Cart {
+    private int id;
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private double totalPrice;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the orderId
+	 */
+	public int getOrderId() {
+		return orderId;
+	}
+
+	/**
+	 * @param orderId the orderId to set
+	 */
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	/**
+	 * @return the productId
+	 */
+	public int getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the totalPrice
+	 */
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	/**
+	 * @param totalPrice the totalPrice to set
+	 */
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+}
